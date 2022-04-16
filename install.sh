493093dcdb3911564a365dc9aae0ad8103c5a273
Craig Chambers <craigchambers2101@gmail.com>
2022-04-16 10:14:20 +0100

    Install GTK theme

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# GTK Theme
+wget -P $HOME/Downloads/ https://github.com/dracula/gtk/archive/master.zip
+cd Downloads
+sudo unzip master.zip -d /usr/share/themes
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
