0dae521908f9e1bea60eeb86c423a114c8631f1d
Craig Chambers <craigchambers2101@gmail.com>
2022-04-16 11:05:35 +0100

    Added Tmux and Xresources

--- install.sh
+++ install.sh
@@ -13,3 +13,19 @@ cd /tmp
 git clone https://github.com/dracula/gnome-terminal
 cd gnome-terminal
 bash install.sh
+
+# Vim Theme
+# Installed via dotfiles install script
+
+# TMUX Theme
+mkdir -p $HOME/.tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo '# List of plugins' >> $HOME/.tmux.conf
+echo "set -g @plugin 'tmux-plugins/tpm'" >> $HOME/.tmux.conf
+echo "set -g @plugin 'tmux-plugins/tmux-sensible'" >> $HOME/.tmux.conf
+echo '# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)' >> $HOME/.tmux.conf
+echo 'run $HOME/.tmux/plugins/tpm/tpm' >> $HOME/.tmux.conf
+
+# XResources
+git clone https://github.com/dracula/xresources.git ~/.Xresources
+
