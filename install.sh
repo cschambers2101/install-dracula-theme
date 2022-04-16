b1ea50a31fe864af949339b44691a22a1f51ffc9
Craig Chambers <craigchambers2101@gmail.com>
2022-04-16 10:23:11 +0100

    Added Gnome Terminal install

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 wget -P /tmp/ https://github.com/dracula/gtk/archive/master.zip
 cd Downloads
 sudo unzip /tmp/master.zip -d /usr/share/themes
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+gsettings set org.gnome.desktop.interface gtk-theme "gtk-master"
+gsettings set org.gnome.desktop.wm.preferences theme "gtk-master"
 
+# Gnome-Terminal
+sudo apt-get install dconf-cli
+cd /tmp
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+bash install.sh
