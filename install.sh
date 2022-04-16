a189fc7e6da77d59daf86e3f6774a9d449e936ed
Craig Chambers <craigchambers2101@gmail.com>
2022-04-16 10:17:47 +0100

    Used tmp directory instead of downloads

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # GTK Theme
-wget -P $HOME/Downloads/ https://github.com/dracula/gtk/archive/master.zip
+wget -P /tmp/ https://github.com/dracula/gtk/archive/master.zip
 cd Downloads
-sudo unzip $HOME/Downloads/master.zip -d /usr/share/themes
+sudo unzip /tmp/master.zip -d /usr/share/themes
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
 
